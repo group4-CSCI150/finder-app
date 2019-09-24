cdbe6fc34198076267dcff29e4dd6f4b9fec38bf
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2019-09-24 15:04:00 -0700

    React Native and React Navigation Samples

--- App.js
+++ App.js
@@ -1,56 +1,91 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+/* Sample Apps */
+import NavigatorSampleScreen from './samples/Navigator.js';
+import TextInputSampleScreen from './samples/TextInput.js';
+import ButtonSampleScreen from './samples/Button.js';
+import ScrollViewSampleScreen from './samples/ScrollView.js';
+import FlatListSampleScreen from './samples/FlatList.js';
+
+import DrawerApp from './DrawerApp.js';
+import AuthApp from './AuthApp.js';
+
 class HomeScreen extends React.Component {
+  
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button
+          title="Navigator Sample"
+          onPress={() => this.props.navigation.navigate('NavigatorSample')}
+        />
+        <Button
+          title="Text Input Sample"
+          onPress={() => this.props.navigation.navigate('TextInputSample')}
+        />
+        <Button
+          title="Button Sample"
+          onPress={() => this.props.navigation.navigate('ButtonSample')}
+        />
+        <Button
+          title="ScrollView Sample"
+          onPress={() => this.props.navigation.navigate('ScrollViewSample')}
+        />
+        <Button
+          title="FlatList Sample"
+          onPress={() => this.props.navigation.navigate('FlatListSample')}
+        />
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
-
-const AppNavigator = createStackNavigator({
+// Create stack navigator with two screens
+const AppNavigator = createStackNavigator(
+  {
     Home: {
       screen: HomeScreen,
     },
-    Details: {
-      screen: DetailsScreen,
+    NavigatorSample: {
+      screen: NavigatorSampleScreen,
+    },
+    TextInputSample: {
+      screen: TextInputSampleScreen,
+    },
+    ButtonSample: {
+      screen: ButtonSampleScreen,
+    },
+    ScrollViewSample: {
+      screen: ScrollViewSampleScreen,
+    },
+    FlatListSample: {
+      screen: FlatListSampleScreen,
     }
   }, 
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
 
 const AppContainer = createAppContainer(AppNavigator);
 
-
-export default class App extends React.Component {
+class ReactSamplesApp extends React.Component {
   render() {
     return <AppContainer />;
   }
 }
-/*
-export default class HelloWorldApp extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, worlddddd</Text>
-      </View>
-    );
-  }
-}*/
+
+export default AuthApp;
---
+++ DrawerApp.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+class MyHomeScreen extends React.Component {
+    static navigationOptions = {
+      drawerLabel: 'Home',
+      drawerIcon: ({ tintColor }) => (
+        <Image
+          source={require('./images/bulldog.png')}
+          style={[styles.icon, { tintColor: tintColor }]}
+        />
+      ),
+    };
+  
+    render() {
+      return (
+        <Button
+          onPress={() => this.props.navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+      );
+    }
+  }
+  
+  class MyNotificationsScreen extends React.Component {
+    static navigationOptions = {
+      drawerLabel: 'Notifications',
+      drawerIcon: ({ tintColor }) => (
+        <Image
+          source={require('./images/bulldog.png')}
+          style={[styles.icon, { tintColor: tintColor }]}
+        />
+      ),
+    };
+  
+    render() {
+      return (
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    icon: {
+      width: 24,
+      height: 24,
+    },
+  });
+  
+  const MyDrawerNavigator = createDrawerNavigator({
+    Home: {
+      screen: MyHomeScreen,
+    },
+    Notifications: {
+      screen: MyNotificationsScreen,
+    },
+  });
+  
+  const MyApp = createAppContainer(MyDrawerNavigator);
+
+  export default MyApp;
