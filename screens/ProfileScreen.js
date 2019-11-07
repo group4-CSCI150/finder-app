1c4a711da8d0c68001922ba2799d37f9149ac63d
Leonardo <yoshida.leoy@gmail.com>
2019-11-07 14:45:30 -0800

    Call api with updated user. Add loading icon. Load user and set state using token. TODO: get data from api istead of token

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,86 +1,129 @@
 import * as WebBrowser from 'expo-web-browser';
-import React, {Component} from 'react';
-import {StyleSheet,
-        Text,
-        TextInput,
-        ScrollView,
-        View,
-        Image,
-        TouchableOpacity,
-        KeyboardAvoidingView,
-      } from 'react-native'; 
 
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  ScrollView,
+  View,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ActivityIndicator
+} from 'react-native';
 
-export default class profilePage extends Component{
-  constructor(props){
+import api from '../utils/apiCaller'
+import token from '../utils/tokenFunctions'
+
+export default class profilePage extends Component {
+  constructor(props) {
     super(props);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleSave = this.handleSave.bind(this);
-
 
     this.state = {
       name: "Name",
       desc: "Bio",
       tag: "Interest",
+      username: "",
       isEdit: false,  // editablity of text input - true when edit
       isMulti: true,  // make able to do multiple lines when input text
       buttonName: "Edit Profile", // default - on button press, title will change to confirm changes
       pressStatus: false, // for changing color of button when edit
       changeButton: false, // change button style depending on if view or edit screen
+      loading: false
     };
   }
- 
-  handleEdit(){       // if isEdit initially false -> switch to edit page
-    this.setState({isEdit: true});
-    this.setState({buttonName: "Confirm"});
-    this.setState({pressStatus: true}); // change to edit text style
-    this.setState({changeButton: true});  // confirm changes button style
+
+  //TODO: Instead of getting user data from token, use the token to get username. 
+  //      Then, call API using username to get updated user data 
+  getToken(){
+    return token.getToken();
   }
 
-  handleSave(){     // if isEdit initially true -> confirm changes and switch to viewing
-    this.setState({isEdit: false});
-    this.setState({buttonName: "Edit Profile"});
-    this.setState({pressStatus: false});  // change to view only style
-    this.setState({changeButton: false}); // edit button style
+  async componentDidMount() {
+    let tok = await this.getToken();
+    tok = JSON.parse(tok).user
+
+    this.setState(
+      {
+        name: tok.name,
+        desc: tok.des,
+        tag: JSON.stringify(tok.tags),
+        username: tok.username
+      }
+    )
+
   }
 
- // display view profile as default - on button press, be able to edit the text fields
-  render(){
+  handleEdit() {       // if isEdit initially false -> switch to edit page
+    this.setState({ isEdit: true });
+    this.setState({ buttonName: "Confirm" });
+    this.setState({ pressStatus: true }); // change to edit text style
+    this.setState({ changeButton: true });  // confirm changes button style
+  }
+
+  async handleSave() {     // if isEdit initially true -> confirm changes and switch to viewing
+    
+
+    let updatedUser = {
+      name: this.state.name,
+      des: this.state.desc,
+      tags: this.state.tags
+    }
+    this.setState({loading: true})
+    let update = await api.updateUser(this.state.username, updatedUser)
+    this.setState({loading: false})
+
+    this.setState({ isEdit: false });
+    this.setState({ buttonName: "Edit Profile" });
+    this.setState({ pressStatus: false });  // change to view only style
+    this.setState({ changeButton: false }); // edit button style
+  }
+
+  // display view profile as default - on button press, be able to edit the text fields
+  render() {
+    let loading;
+    if(this.state.loading){
+      loading = <ActivityIndicator size="large" color="#0000ff" />
+    }
+
     return (
       <KeyboardAvoidingView style={style.container} behavior="padding">
-      <View style={style.header}>
-        <Text style={style.headerFont}>My Profile</Text>       
-    
-        <View style={style.profilepicWrap}>
-            <Image source={require('../images/bulldog.png')} style={style.profilepic}/>
-        </View>   
+        <View style={style.header}>
+          <Text style={style.headerFont}>My Profile</Text>
 
-   
-        <TextInput editable={this.state.isEdit} 
-                  multiline={this.state.isMulti}
-                  style={this.state.pressStatus ? style.textInput_style: style.default_profile}
-                  onChangeText={(name) => this.setState({name})}> 
-                      {this.state.name}</TextInput>
-        <TextInput editable={this.state.isEdit} 
-                  multiline={this.state.isMulti}
-                  style={this.state.pressStatus ? style.textInput_style: style.default_profile}
-                  onChangeText={(desc) => this.setState({desc})}>
-                      {this.state.desc}</TextInput>
-        <TextInput editable={this.state.isEdit}
-                  multiline={this.state.isMulti} 
-                  style={this.state.pressStatus ? style.textInput_style: style.default_profile}
-                  onChangeText={(tag) => this.setState({tag})}>
-                      {this.state.tag}</TextInput>
-        
-        <TouchableOpacity style={this.state.changeButton ? style.buttonConfirmContainer: style.buttonEditContainer} 
-                          onPress={this.state.isEdit ? this.handleSave: this.handleEdit}>
-        <Text style={style.buttonFont}>{this.state.buttonName}</Text>
-        </TouchableOpacity>
-      
-      </View>
+          <View style={style.profilepicWrap}>
+            <Image source={require('../images/bulldog.png')} style={style.profilepic} />
+          </View>
+
+
+          <TextInput editable={this.state.isEdit}
+            multiline={this.state.isMulti}
+            style={this.state.pressStatus ? style.textInput_style : style.default_profile}
+            onChangeText={(name) => this.setState({ name })}>
+            {this.state.name}</TextInput>
+          <TextInput editable={this.state.isEdit}
+            multiline={this.state.isMulti}
+            style={this.state.pressStatus ? style.textInput_style : style.default_profile}
+            onChangeText={(desc) => this.setState({ desc })}>
+            {this.state.desc}</TextInput>
+          <TextInput editable={this.state.isEdit}
+            multiline={this.state.isMulti}
+            style={this.state.pressStatus ? style.textInput_style : style.default_profile}
+            onChangeText={(tag) => this.setState({ tag })}>
+            {this.state.tag}</TextInput>
+
+          <TouchableOpacity style={this.state.changeButton ? style.buttonConfirmContainer : style.buttonEditContainer}
+            onPress={this.state.isEdit ? this.handleSave : this.handleEdit}>
+            <Text style={style.buttonFont}>{this.state.buttonName}</Text>
+          </TouchableOpacity>
+          {loading}
+        </View>
       </KeyboardAvoidingView>
-     
-      );
+
+    );
   }
 
 }
@@ -100,17 +143,17 @@ const style = StyleSheet.create({
     backgroundColor: '#dc143c',
     alignSelf: "center",
     justifyContent: "center",
-    marginTop: 70,    
+    marginTop: 70,
     width: '60%'
   },
 
   buttonConfirmContainer: {
     height: 30,
-    paddingVertical: 20, 
+    paddingVertical: 20,
     backgroundColor: '#214786',
     alignSelf: "center",
     justifyContent: "center",
-    marginTop: 45,    
+    marginTop: 45,
     width: '60%'
   },
 
@@ -119,7 +162,7 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     padding: 15,
-    backgroundColor: '#ffffff',  
+    backgroundColor: '#ffffff',
     borderColor: '#214786',
     borderWidth: 5
   },
@@ -127,7 +170,7 @@ const style = StyleSheet.create({
   profilepicWrap: { // square profile pic
     width: 225,
     height: 225,
-    borderColor: '#214786',   
+    borderColor: '#214786',
     borderWidth: 1
   },
 
@@ -136,27 +179,27 @@ const style = StyleSheet.create({
     width: null,
     alignSelf: 'stretch',
     borderRadius: 100,
-    borderColor: '#fff',    
+    borderColor: '#fff',
     borderWidth: 4
   },
 
   textInput_style: {
     height: 40,
     width: 250,
-    margin: 5,  
-    padding: 5, 
+    margin: 5,
+    padding: 5,
     borderColor: 'black',
     borderWidth: 1,
-    fontSize: 20, 
+    fontSize: 20,
   },
-    
-// Text styles
+
+  // Text styles
   headerFont: {
     padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 32,
-    color: '#000',   
+    color: '#000',
     fontWeight: 'bold'
   },
 
@@ -165,7 +208,7 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 22,
-    color: '#000',   
+    color: '#000',
   },
 
   buttonFont: {
