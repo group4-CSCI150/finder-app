53a3cd7369fa8d7fff569b6f49eb05c7b710504e
Leonardo <yoshida.leoy@gmail.com>
2019-11-12 14:29:40 -0800

    Conditional rendering for options menu

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import {
     Text,
     TouchableOpacity,
     StyleSheet
-} from 'react-native'; 
+} from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
 import OptionsModal from './OptionsModal';
@@ -17,13 +17,13 @@ function Header(props) {
 
     [modalVisible, setModalVisible] = useState(false);
 
-    
+
     // Add back button if requested by props
     if (props.back) {
-        var leftContent = 
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 10}}>
-                <TouchableOpacity onPress={() => { if (props.navigation) props.navigation.goBack()}}>
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+        var leftContent =
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 10 }}>
+                <TouchableOpacity onPress={() => { if (props.navigation) props.navigation.goBack() }}>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
                         <Ionicons name="ios-arrow-back" size={HEADER_HEIGHT} />
                         <Text style={styles.headerText}>Back</Text>
                     </View>
@@ -31,22 +31,24 @@ function Header(props) {
             </View>
     }
 
-    var middleContent = 
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    var middleContent =
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={styles.headerText}>{props.title ? props.title : 'Finder'}</Text>
         </View>
-
-    var rightContent = 
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', marginRight: 10}}>
-                <TouchableOpacity onPress={() => {setModalVisible(true);}}>
+        
+    if (props.actions) {
+        var rightContent =
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', marginRight: 10 }}>
+                <TouchableOpacity onPress={() => { setModalVisible(true); }}>
                     <Ionicons name="md-cog" size={HEADER_HEIGHT} color="black" />
                 </TouchableOpacity>
             </View>;
+    }
 
     return (
-        <View style={{width: '100%', height: HEADER_HEIGHT}}>
+        <View style={{ width: '100%', height: HEADER_HEIGHT }}>
             <OptionsModal modalVisible={modalVisible} setModalVisible={setModalVisible} actions={props.actions} />
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
                 <View style={styles.headerContent}>
                     {leftContent}
                 </View>
@@ -68,7 +70,7 @@ export default withNavigation(Header);
 var styles = StyleSheet.create({
     headerContent: {
         width: '33%',
-        height: HEADER_HEIGHT, 
+        height: HEADER_HEIGHT,
     },
     headerText: {
         fontSize: HEADER_HEIGHT * 0.5,
