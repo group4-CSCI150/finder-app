39ffd6dd79590fdbf47d6ff640331189c4902508
Leonardo <yoshida.leoy@gmail.com>
2019-11-19 14:48:24 -0800

    add catch blocks and create calls for finding recomendations and friendlist

--- utils/apiCaller.js
+++ utils/apiCaller.js
@@ -1,3 +1,5 @@
+import token from "./tokenFunctions"
+
 const axios = require('axios')
 
 const baseURL = 'https://us-central1-test150project.cloudfunctions.net/api/user/'
@@ -8,42 +10,98 @@ body is a JSON object. e.g. {username: "Leo", password: "myPassword", email: "my
 */
 
 const api = {
+  // Gets current loggedin user
+  getCurrentUser: async () => {
+    try {
+      let username = await token.getToken()
+      username = JSON.parse(username).user.username
+      console.log(username)
+      let user = await axios.get(`${baseURL}byID/${username}`)
+      console.log(user.data)
+      return user.data.user
+    }catch{
+      return "Error getting user"
+    }
+
+  },
 
   // Returns a single user from the DB
   getUserById: async (username) => {
-    let user = await axios.get(`${baseURL}byID/${username}`)
-    console.log("GET: ", user.data)
-    return user.data
+    try {
+      let user = await axios.get(`${baseURL}byID/${username}`)
+      console.log("GET: ", user.data)
+      return user.data
+    }catch{
+      return "Error getting user"
+    }
   },
 
   // Returns a list of ALL users in the DB (Probably not needed)
   getAllUser: async () => {
-    let user = await axios.get(`${baseURL}`)
-    console.log("GET: ", user.data)
-    return user.data
+    try {
+      let user = await axios.get(`${baseURL}`)
+      console.log("GET: ", user.data)
+      return user.data
+    }catch{
+      return "Error getting user"
+    }
   },
 
   // Creates a user in the DB. In this case the body MUST contain an attribute username and password
   createUser: async (body) => {
-    let user = await axios.post(`${baseURL}`, body)
-    console.log("POST: ", user.data)
-    return user.data
+    try {
+      let user = await axios.post(`${baseURL}`, body)
+      console.log("POST: ", user.data)
+      return user.data
+    }catch{
+      return "Error getting user"
+    }
   },
 
   // Updates a user in the DB
   updateUser: async (username, body) => {
-    let user = await axios.put(`${baseURL}byID/${username}`, body)
-    console.log("PUT: ", user.data)    
-    return user.data
-
+    try {
+      let user = await axios.put(`${baseURL}byID/${username}`, body)
+      console.log("PUT: ", user.data)
+      return user.data
+    }catch{
+      return "Error getting user"
+    }
   },
 
   // Check if username and password match the ones in the DB then return if match.
-  callLogin: async (body)=> {
-    let user = await axios.post(`${baseURL}login`, body)
-    console.log("LOGIN: ", user.data)
-    return user.data
+  callLogin: async (body) => {
+    try {
+      let user = await axios.post(`${baseURL}login`, body)
+      console.log("LOGIN: ", user.data)
+      return user.data
+    }catch{
+      return "Error getting user"
+    }
   },
+
+  getFriends: async () => {
+    try {
+      let username = await token.getToken()
+      username = JSON.parse(username).user.username
+      let friends = await axios.get(`${baseURL}byIDList/${username}`)
+      console.log("GET Friends: ", friends.data)
+      return friends.data
+    }catch{
+      return "Error getting user"
+    }
+  },
+
+  getRecomendations: async (body) => {
+    try {
+      let rec = await axios.post(`${baseURL}finder`, body)
+      console.log("Recommendations: ", rec.data)
+      return rec.data
+    }catch{
+      console.log("ERROR")
+      return "Error getting user"
+    }
+  }
 }
 
 export default api;
