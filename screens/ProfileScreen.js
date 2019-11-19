b008f57fa97b725c7d4c86ee0a1822ae890d608d
unknown <selenav17@mail.fresnostate.edu>
2019-11-19 13:32:55 -0800

    Dropdown menu for tags ver.1 - can only choose 1

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -12,28 +12,36 @@ import {
   KeyboardAvoidingView,
   ActivityIndicator
 } from 'react-native';
+import PropTypes from 'prop-types';
+import {Select, SelectionBox, Option, OptionList} from 'react-native-chooser';
 
 import api from '../utils/apiCaller'
 import token from '../utils/tokenFunctions'
 
+//var selectedTags= []; // selected tags from dropdown
 export default class profilePage extends Component {
   constructor(props) {
     super(props);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleSave = this.handleSave.bind(this);
-
+   
     this.state = {
       name: "Name",
       desc: "Bio",
-      tag: "Interest",
+      tag: "Interests",
       username: "",
       isEdit: false,  // editablity of text input - true when edit
       isMulti: true,  // make able to do multiple lines when input text
       buttonName: "Edit Profile", // default - on button press, title will change to confirm changes
       pressStatus: false, // for changing color of button when edit
       changeButton: false, // change button style depending on if view or edit screen
-      loading: false
+      loading: false,
+
+      // sample list of tags
+      listTags: [ "[\"art\"]", "[\"coding\"]", "[\"sports\"]", "[\"reading\"]",],
+     
     };
+  
   }
 
   //TODO: Instead of getting user data from token, use the token to get username. 
@@ -62,11 +70,10 @@ export default class profilePage extends Component {
     this.setState({ buttonName: "Confirm" });
     this.setState({ pressStatus: true }); // change to edit text style
     this.setState({ changeButton: true });  // confirm changes button style
+  //  this.setState({selectedTags});
   }
 
   async handleSave() {     // if isEdit initially true -> confirm changes and switch to viewing
-    
-
     let updatedUser = {
       name: this.state.name,
       des: this.state.desc,
@@ -80,6 +87,11 @@ export default class profilePage extends Component {
     this.setState({ buttonName: "Edit Profile" });
     this.setState({ pressStatus: false });  // change to view only style
     this.setState({ changeButton: false }); // edit button style
+  }
+
+  onSelect(value) {
+    this.setState({tag: value});
+ //   selectedTags.push(this.state.value);
   }
 
   // display view profile as default - on button press, be able to edit the text fields
@@ -104,16 +116,28 @@ export default class profilePage extends Component {
             style={this.state.pressStatus ? style.textInput_style : style.default_profile}
             onChangeText={(name) => this.setState({ name })}>
             {this.state.name}</TextInput>
+
           <TextInput editable={this.state.isEdit}
             multiline={this.state.isMulti}
             style={this.state.pressStatus ? style.textInput_style : style.default_profile}
             onChangeText={(desc) => this.setState({ desc })}>
             {this.state.desc}</TextInput>
-          <TextInput editable={this.state.isEdit}
-            multiline={this.state.isMulti}
-            style={this.state.pressStatus ? style.textInput_style : style.default_profile}
-            onChangeText={(tag) => this.setState({ tag })}>
-            {this.state.tag}</TextInput>
+
+            <Select 
+              onSelect = {this.onSelect.bind(this)}
+              defaultText = {this.state.tag} 
+              //backgroundcolor = {'black'}
+              textStyle = {this.state.isEdit ? style.tagFont_edit : style.tagFont_prof}
+              style = {this.state.pressStatus ? style.tagInput_style : style.default_tag}
+            >
+              {this.state.listTags.map(listTags => (
+                <Option 
+                  key={listTags} value={listTags}>
+                  {listTags}
+                </Option>
+              ))}
+            </Select>
+            
 
           <TouchableOpacity style={this.state.changeButton ? style.buttonConfirmContainer : style.buttonEditContainer}
             onPress={this.state.isEdit ? this.handleSave : this.handleEdit}>
@@ -127,7 +151,6 @@ export default class profilePage extends Component {
   }
 
 }
-
 
 
 // STYLE
@@ -193,6 +216,28 @@ const style = StyleSheet.create({
     fontSize: 20,
   },
 
+  tagInput_style: {
+    height: 40,
+    width: 250,
+    margin: 5,
+    padding: 5,
+    borderColor: 'black',
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+
+  default_tag: {
+    height: 40,
+    width: 250,
+    margin: 5,
+    padding: 5,
+    borderColor: 'white',
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+
   // Text styles
   headerFont: {
     padding: 10,
@@ -215,6 +260,22 @@ const style = StyleSheet.create({
     textAlign: "center",
     color: '#fff',
     fontSize: 20
+  },
+
+  tagFont_prof: {
+    textAlign: "center",
+    justifyContent: 'center',
+    alignSelf: 'center',
+    color: 'black',
+    fontSize: 22,
+  },
+
+  tagFont_edit: {
+    textAlign: "center",
+    justifyContent: 'center',
+    alignSelf: 'center',
+    color: 'black',
+    fontSize: 20,
   },
 });
 
