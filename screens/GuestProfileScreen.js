3ad09ab38fda3fc0a15e5033ad6dfa1a78709979
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2019-11-19 21:29:38 -0800

    Added navigation parameters

--- screens/GuestProfileScreen.js
+++ screens/GuestProfileScreen.js
@@ -4,6 +4,7 @@ import {
     View,
     Text,
     ScrollView,
+    Platform,
 } from 'react-native';
 
 import ImageSwiper from '../components/ImageSwiper';
@@ -12,50 +13,61 @@ import Header from '../components/Header';
 // TODO: Style with react-native-linear-gradient
 function Divider(props) {
     return (
-        <View style={{width: '100%', height: 5, backgroundColor: 'black', borderRadius: 2.5}} />
+        <View style={{width: '100%', height: 5}}>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{width: '75%', height: 5, backgroundColor: 'black', borderRadius: 2.5}} />
+            </View>
+        </View>
     );
 }
 
-
 /*
-    Props:
-    user - an object containing all information required in a user profile.
+Navigation Parameters:
+    user - Object, required. Contains all information required in a user profile.
     Is of the form:
     {
-        name: String. User's full name,
-
+        name: String. User's full name
+        tags: Array of Strings.
+        description: String. User's profile description
+        major: String. User's major
     }
 */
 export default function GuestProfileScreen(props) {
+    var user = props.navigation.getParam('user', {});
+    
+    var name = user.name ? user.name : 'No name';
+    var tags = user.tags ? user.tags.join(', ') : 'No tags';
     return (
-        <SafeAreaView>
-            <Header back={true} actions={[{name: 'Test'}]} />
-            <ImageSwiper />
-            <View>
-                <View>
-                    <Text>{props.user.name}</Text>
+        <SafeAreaView style={{marginBottom: 50}}>
+            <Header back={true} actions={[{name: 'Add Friend', iconName: Platform.OS === 'ios' ? 'ios-person-add' : 'md-person-add'}]} />
+            <ScrollView>
+                <ImageSwiper height={400} imgUris={['uri1', 'uri2', 'uri3']} />
+                <View style={{margin: 20}}>
+                    <View>
+                        <Text>{name}</Text>
+                    </View>
+                    <View>
+                        <Text>{user.major ? props.user.major : 'No major'}</Text>
+                    </View>
+                    <View>
+                        <Text>{tags}</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text>{props.user.major}</Text>
+                <Divider />
+                <View style={{margin: 20}}>
+                    <Text>{user.description ? user.description : 'No description'}</Text>
                 </View>
-                <View>
-                    <Text>{props.user.tags}</Text>
+                <Divider />
+                <View style={{margin: 20}}>
+                    <View>
+                        <Text>{name} likes:</Text>
+                    </View>
+                    <View>
+                        <Text>{tags}</Text>
+                    </View>
                 </View>
-            </View>
-            <Divider />
-            <View>
-                <Text>{props.user.description}</Text>
-            </View>
-            <View>
-                <View>
-                    <Text>{props.user.name} likes:</Text>
-                </View>
-                <View>
-                    <Text>{props.user.tags}</Text>
-                </View>
-            </View>
-
-            <View><Text>Add Friend!</Text></View>
+                <View><Text>Add Friend!</Text></View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
