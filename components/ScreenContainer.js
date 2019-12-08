b01bf88050fbcd1d28fa2bb5ffb46ed0ca4f2204
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2019-12-07 23:16:02 -0800

    Added container for Screens

---
+++ components/ScreenContainer.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import {
+    View,
+    Animated,
+    Easing
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+export default function ScreenContainer(props) {
+    return (
+        <LinearGradient style={{flex: 1, minHeight: '100%'}} colors={["#8A0012", "#300059"]} start={[0, 0]} end={[0, 1]}>
+            {props.children}
+        </LinearGradient>
+    );
+}
