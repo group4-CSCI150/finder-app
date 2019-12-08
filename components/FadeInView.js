e78be6dcbd8f3010fa276bcec224c3784f3505e1
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2019-12-07 23:14:21 -0800

    Added FadeInView component

---
+++ components/FadeInView.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import {
+    View,
+    Animated,
+    Easing
+} from 'react-native';
+
+
+export default function FadeInView(props) {
+    const [opacity] = useState(new Animated.Value(0));
+  
+    useEffect(() => {
+      Animated.timing(
+        opacity,
+          {
+            toValue: 1,
+            duration: 500,
+            easing: Easing.ease,
+          }
+      ).start();
+    }, []);
+  
+    return (
+      <Animated.View
+        style={{
+          ...props.style,
+          opacity: opacity,
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  }
